214ee0216e91881fd74c072edb4054fc9cd30dfe
favial-cyril <favian.cyril@gmail.com>
2016-05-04 01:59:20 +0700

    'fix'

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -24,10 +24,10 @@ class UploadController extends Controller
            DB::table('users')->where('id', $user->id)->update(['profilepict' => 'public/asset/' . $filename]);
            Request::file('fileToUpload')->move(public_path().'/asset', $filename);
            Session::flash('status', 'Upload successfully');
-           return Redirect::to('upload');
+           return back();
          } else {
            Session::flash('status', 'uploaded file is not valid');
-           return Redirect::to('upload');
+           return back();
          }
       }
     }//
